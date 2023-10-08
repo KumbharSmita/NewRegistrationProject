88591e40fe107a42c5b990b1e96f7a1e70f8ed8d
KumbharSmita <141403037+KumbharSmita@users.noreply.github.com>
2023-10-08 13:46:30 +0530

    Add files via upload

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms'
+import{ToastrService} from 'ngx-toastr'
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  constructor(private builder: FormBuilder,private toastr:ToastrService,
+    private service:AuthService,private router:Router){
+
+  }
+  userdata:any;
+  loginform=this.builder.group({
+    username: this.builder.control('', Validators.required),
+
+    password: this.builder.control('', Validators.required)
+
+
+  })
+
+  proceedlogin(){
+    if(this.loginform.valid){
+
+    //   this.service.Proceedregistration(this.loginform.value).subscribe(res=>{
+    //       this.toastr.success('Please Contact Admin for enable Access','Registerd successfully')
+    //       this.router.navigate(['login']);
+    //   });
+
+    // }
+    // else{
+    //   this.toastr.warning('Please enter valid data');
+    this.service.GetbyCode(this.loginform.value.username).subscribe(res=>{
+      this.userdata=res;
+      console.log(this.userdata);
+
+
+    })
+    }
+  }
+
+}
